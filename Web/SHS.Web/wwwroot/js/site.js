70127cfdb7dc13dad3ede9f0db25f2849354e1b2
沈平元 <SPY2439739932@outlook.com>
2020-04-30 11:52:20 +0800

    开始做登陆注册逻辑

--- Web/SHS.Web/wwwroot/js/site.js
+++ Web/SHS.Web/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+
+    $(".login-submit").click(function () {
+        $("img.login-loading").css("display", "inline-block");
+        $.ajax({
+            url: "/Account/Login",
+            type: "POST",
+            data: {
+                UserName: $(".form-login #username").val(),
+                Password: $(".form-login #password").val(),
+            },
+            asycn: false,
+            dataType: "json",
+            beforeSend: function (res) {
+            },
+            success: function (res) {
+                if (res.code == 0) {
+                    $(".form-login .loading>p").html("登陆成功");
+                    setTimeout(' window.location.href = "/Home/Index"', 1000);
+                    // window.location.href = "/Home/Index";
+                } else {
+                    setTimeout('$(".form-login .loading").css("display", "none")', 2000);
+                }
+            }
+        });
+    })
+})
