5cb5949d3fb83559be5f4ba54dd47aa9f76e0653
沈平元 <SPY2439739932@outlook.com>
2020-05-01 12:00:26 +0800

    头皮发麻，吃个午饭再来

--- Web/SHS.Web/wwwroot/js/site.js
+++ Web/SHS.Web/wwwroot/js/site.js
@@ -7,13 +7,11 @@ $(function () {
     $(".login-submit").click(function () {
         $("img.login-loading").css("display", "inline-block");
         $.ajax({
-            url: webApiHost+"api/Account/LoginApi",
+            url: webApiHost + "api/Account/LoginApi",
             type: "POST",
             data: JSON.stringify({
-                //"username":$(".form-login #username").val(),
-                //"password": $(".form-login #password").val(),
-                userName: "123",
-                password: "456"
+                "username": $(".form-login input[name='username']").val(),
+                "password": $(".form-login input[name='password']").val(),
             }),
             contentType: "application/json;",
             asycn: false,
@@ -29,6 +27,91 @@ $(function () {
                 } else {
                     $("input.login-submit").val(res.msg);
                     setTimeout('$("input.login-submit").val("登陆")', 2000);
+                }
+            },
+            error: function (res) {
+                window.location.href = "/html/error.html";
+            }
+        });
+    });
+    //$(".form-register").validate({
+    //    submitHandler: function (form) {
+    //        $(".register-submit").click(function () {
+    //            if ($(".form-register input[name='passwordsignup']").val() != $(".form-register input[name='passwordsignup_confirm']").val()) {
+    //                $("input.register-submit").val("两密码不同");
+    //                setTimeout('$("input.register-submit").val("注册")', 2000);
+    //                return false;
+    //            }
+    //            $("img.register-loading").css("display", "inline-block");
+    //            $.ajax({
+    //                url: webApiHost + "api/Account/RegisterApi",
+    //                type: "POST",
+    //                data: JSON.stringify({
+    //                    "username": $(".form-register input[name='username']").val(),
+    //                    "password": $(".form-register input[name='passwordsignup']").val(),
+    //                    "email": $(".form-register input[name='emailsignup']").val(),
+
+    //                }),
+    //                contentType: "application/json;",
+    //                asycn: false,
+    //                dataType: "json",
+    //                beforeSend: function (res) {
+    //                },
+    //                success: function (res) {
+    //                    $("img.register-loading").css("display", "none");
+    //                    if (res.code == 0) {
+    //                        $("input.register-submit").val("注册成功");
+    //                        setTimeout(' window.location.href = "/Home/Index"', 1000);
+    //                        // window.location.href = "/Home/Index";
+    //                    } else {
+    //                        $("input.register-submit").val(res.msg);
+    //                        setTimeout('$("input.register-submit").val("注册")', 2000);
+    //                    }
+    //                },
+    //                error: function (res) {
+    //                    window.location.href = "/html/error.html";
+    //                }
+    //            });
+    //        })
+    //    }
+    //});
+    $(".register-submit").click(function () {
+        if ($(".form-register input[name='usernamesignup']").val() == "" ||
+            $(".form-register input[name='emailsignup']").val() == "" ||
+            $(".form-register input[name='passwordsignup']").val() == "" ||
+            $(".form-register input[name='passwordsignup_confirm']").val() == "") {
+            return true;
+        }
+        if ($(".form-register input[name='passwordsignup_confirm']").val() != $(".form-register input[name='passwordsignup_confirm']").val()) {
+            $("input.register-submit").val("两密码不同");
+            setTimeout('$("input.register-submit").val("注册")', 2000);
+            return false;
+        }
+        $("img.register-loading").css("display", "inline-block");
+        
+        $.ajax({
+            url: webApiHost + "api/Account/RegisterApi",
+            type: "POST",
+            data: JSON.stringify({
+                "username": $(".form-register input[name='username']").val(),
+                "password": $(".form-register input[name='passwordsignup']").val(),
+                "email": $(".form-register input[name='emailsignup']").val(),
+
+            }),
+            contentType: "application/json;",
+            asycn: false,
+            dataType: "json",
+            beforeSend: function (res) {
+            },
+            success: function (res) {
+                $("img.register-loading").css("display", "none");
+                if (res.code == 0) {
+                    $("input.register-submit").val("注册成功");
+                    setTimeout(' window.location.href = "/Home/Index"', 1000);
+                    // window.location.href = "/Home/Index";
+                } else {
+                    $("input.register-submit").val(res.msg);
+                    setTimeout('$("input.register-submit").val("注册")', 2000);
                 }
             },
             error: function (res) {
