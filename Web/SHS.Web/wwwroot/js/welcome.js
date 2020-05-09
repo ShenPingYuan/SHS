3be8a7919707b25e6027a2de12c304ae64a3c0b2
沈平元 <2439739932@qq.com>
2020-05-09 17:14:42 +0800

    添加备案版权信息

---
+++ Web/SHS.Web/wwwroot/js/welcome.js
@@ -0,0 +1,9 @@
+﻿layui.use(['form'], function () {
+    var $ = layui.jquery;
+
+    var date = new Date();
+    var currentTime = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "  " + date.getHours() + ":"
+        + date.getMinutes() + ":" + date.getSeconds();
+    $(".currentTime").html("当前时间：" + currentTime);
+
+});
